a6ecc8aed144849734176db10a9883736163e25d
Priyanshu Garg <priyanshugarg282@gmail.com>
2025-07-15 23:14:22 +0530

    Add Text Editor project with undo/redo stacks

--- text-editor/index.js
+++ text-editor/index.js
@@ -1,37 +1,100 @@
-// Grab DOM elements
-const editor = document.getElementById('editor');
-const undoBtn = document.getElementById('undo');
-const redoBtn = document.getElementById('redo');
+class Stack{
+    arr_undo=[]
+    arr_redo=[]
+    buffer=4
+    topelement = () =>
+    {
+         return this.arr_undo[this.arr_undo.length-1]
+    }
+    popping = () => {
+        if(this.arr_undo.length!=0)
+        {
+            let top = this.arr_undo.pop()
+            return top
+        }
+        return [-1,""]
+    }
+    
+    pushing = (type,charac) => {
+        if(this.arr_undo.length==0)
+        {
+            if(type=="inserting")
+            {
+                this.arr_undo.push([type,charac])
+            }
+        }
+        else
+        {
+            let top = this.topelement()
+            if(type===top[0] && top[1].length<this.buffer)
+            {
+                let st = this.popping()
+                st[1] = charac + st[1];
+                this.arr_undo.push([type,st[1]])
+            }
+            else
+            {
+                this.arr_undo.push([type,charac])
+            }
+        }
+    }
+    reverse = (s1) => {
+        let s = "";
+        let i=s1.length-1;
+        while(i>=0)
+        {
+            s=s.concat(s1.substring(i,i+1))
+            i--;
+        }
+        return s;
+    }
+}   
+    
+    
 
-// Two stacks for history
-const undoStack = [];
-const redoStack = [];
+const textarea = document.getElementById("textarea")
+const undo = document.getElementById("undo")
+const redo = document.getElementById("redo")
+const stack = new Stack()
 
-let lastValue = '';  // previous snapshot
+textarea.addEventListener("keydown",(e) => {
+    switch(e.key)
+    {
+        case 'Backspace': stack.pushing("deleting",textarea.value.substring(textarea.value.length-1,textarea.value.length));
+        break;
+        default : stack.pushing("inserting",e.key);
+    }
+})
 
-/* -------- Capture typing -------- */
-editor.addEventListener('input', e => {
-  undoStack.push(lastValue);      // save old state
-  lastValue = e.target.value;     // update tracker
-  redoStack.length = 0;           // clear redo history
-});
+undo.addEventListener("click",() => {
+    let temp = stack.popping();
+    if(temp[0]!=-1)
+    {
+        stack.arr_redo.push([temp[0],temp[1]])
+        if(temp[0]=="inserting")
+        {
+            let templen = temp[1].length
+            textarea.value =  textarea.value.substring(0,textarea.value.length-templen)
+        }
+        else
+        {
+            textarea.value+=temp[1]
+        }
+    }
+})
 
-/* -------- Undo -------- */
-undoBtn.addEventListener('click', () => {
-  if (!undoStack.length) return;          // nothing to undo
-  redoStack.push(editor.value);           // push current state to redo
-  const prev = undoStack.pop();           // pop last state
-  editor.value = prev;                    // revert
-  lastValue = prev;
-  editor.focus();
-});
-
-/* -------- Redo -------- */
-redoBtn.addEventListener('click', () => {
-  if (!redoStack.length) return;          // nothing to redo
-  undoStack.push(editor.value);           // push current state to undo
-  const next = redoStack.pop();           // pop redo state
-  editor.value = next;                    // apply
-  lastValue = next;
-  editor.focus();
-});
+redo.addEventListener("click",() => {
+    let temp = stack.arr_redo.pop();
+        if(temp[0]=="inserting")
+        {
+            let s = stack.reverse(temp[1])
+            textarea.value+=s
+            stack.pushing("inserting",temp[1])
+        }
+        else
+        {
+            let templen = temp[1].length
+            stack.pushing("deleting", temp[1])
+            textarea.value =  textarea.value.substring(0,textarea.value.length-templen)
+        }
+})
